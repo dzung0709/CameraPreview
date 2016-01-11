e2afae03e469a12ee6b8e655f3639cd34638423f
Dzung Vo <vo.tuan.dung@framgia.com>
2016-01-11 09:40:22 +0700

    Initial commit

---
+++ app/src/obj/local/mips/objs/gpuimage-library/yuv-decoder.o.d
@@ -0,0 +1,17 @@
+F:/Projects/Android/NDKTest/app/src//obj/local/mips/objs/gpuimage-library/yuv-decoder.o: \
+ F:/Projects/Android/NDKTest/app/src//jni/yuv-decoder.c \
+ F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/jni.h \
+ F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/sys/cdefs.h \
+ F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/sys/cdefs_elf.h \
+ F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/android/api-level.h \
+ F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/android/log.h
+
+F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/jni.h:
+
+F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/sys/cdefs.h:
+
+F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/sys/cdefs_elf.h:
+
+F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/android/api-level.h:
+
+F:/Utilities/Software/android-ndk-Win64/platforms/android-L/arch-mips/usr/include/android/log.h:
